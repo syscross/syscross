1d0421773db4894a1615a8f3dfa79040e7f36002
syscross <syscross@proton.me>
2026-09-15 20:30:15 +0900

    sync

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git pull # git clone https://github.com/syscross/syscross
 cd ..
 
 git clone https://github.com/syscross/syscross-dev
-git clone https://github.com/syscross/cargo-txx
-git clone https://github.com/syscross/cargo-rsx
+git clone https://github.com/syscross/txx
+git clone https://github.com/syscross/rsx
 git clone https://github.com/syscross/markx
 git clone https://github.com/syscross/htmlx
